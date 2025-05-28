4a2740ced10b9361ed04c8bbe19f72f675bc3b4a
imerkozhani00 <imer.kozhani.student@ksgyf.se>
2025-05-28 10:21:54 +0200

    Sista commiten där kommentarer lagts till

--- main.ps1
+++ main.ps1
@@ -1,11 +1,14 @@
+# Läs in miljövariabler från .env
 Get-Content .env | ForEach-Object {
     if ($_ -match "^(.*?)=(.*)$") {
         [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2].Trim())
     }
 }
 
+# importera moduler från vmcreator
 Import-Module "$PSScriptRoot\vmcreator.psm1" -Force
 
+# Ber användaren om att ange information som behövs för att skapa en ny VM
 $vmName = Read-Host "Ange namn på VM"
 $cpuCount = Read-Host "Ange antal CPU-kärnor"
 $ramSize = Read-Host "Ange RAM i MB"
@@ -13,6 +16,7 @@ $diskSize = Read-Host "Ange hårddiskstorlek i GB"
 $isoPath = Read-Host "Ange sökväg till ISO-fil"
 
 
+# Skapa och starta en ny VM
 try {
     New-VMConfig -Name $vmName -CPU $cpuCount -RAM $ramSize -Disk $diskSize -ISO $isoPath
     Start-VM -Name $vmName
@@ -21,5 +25,6 @@ try {
     Write-Log -Message "Fel vid skapande av VM: $_"
     Write-Error $_
 }
+
 
 
