d2f72ecb1dda3acc2a49141852ebdef2254a0301
imerkozhani00 <imer.kozhani.student@ksgyf.se>
2025-05-28 10:15:20 +0200

    Den roliga commiten av att skapa och starta VM

--- main.ps1
+++ main.ps1
@@ -13,4 +13,13 @@ $diskSize = Read-Host "Ange hårddiskstorlek i GB"
 $isoPath = Read-Host "Ange sökväg till ISO-fil"
 
 
+try {
+    New-VMConfig -Name $vmName -CPU $cpuCount -RAM $ramSize -Disk $diskSize -ISO $isoPath
+    Start-VM -Name $vmName
+    Write-Log -Message "VM '$vmName' skapades och startades."
+} catch {
+    Write-Log -Message "Fel vid skapande av VM: $_"
+    Write-Error $_
+}
+
 
