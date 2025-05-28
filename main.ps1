800f96e9b1423ba20aff845ea9aa77adda3eff6b
imerkozhani00 <imer.kozhani.student@ksgyf.se>
2025-05-28 10:03:03 +0200

    commit av import-module

--- main.ps1
+++ main.ps1
@@ -3,3 +3,5 @@ Get-Content .env | ForEach-Object {
         [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2].Trim())
     }
 }
+
+Import-Module "$PSScriptRoot\vmceator.psm1" -Force
