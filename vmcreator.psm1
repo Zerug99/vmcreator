45826455f080888c91a7e7c94bf9f247d88c86bc
unknown <daud357@outlook.com>
2025-05-28 10:21:53 +0200

    array med parametrar som behöver vm

--- vmcreator.psm1
+++ vmcreator.psm1
@@ -11,6 +11,30 @@ function New-VMConfig {
         New-Item -ItemType Directory -Path $vmPath -Force Out-Null
 
         $vmxPath = "$vmPath\$Name.vmx"
+        $vmxContent = @(
+            'encoding = "UTF-8"',
+            'config.version = "8"',
+            'virtualHW.version = "16"',
+            "guestOS = ""windows10-64""",
+            "memsize = $RAM",
+            "numvcpus = $CPU",
+            "nvme0.present = ""TRUE""",
+            "nvme0:0.fileName = `"$Name.vmdk`"",
+            "nvme0:0.present = ""TRUE""",
+            'ide1:0.present = "TRUE"',
+            "ide1:0.fileName = `"$ISO`"",
+            "ide1:0.deviceType = ""cdrom-image""",
+            "ethernet0.present = ""TRUE""",
+            "ethernet0.connectionType = ""nat""",
+            "displayName = `"$Name`"",
+            "virtualHW.productCompatibility = ""hosted""",
+            "nvram = `"$Name.nvram`"",
+            "pciBridge0.present = ""TRUE""",
+            "pciBridge4.present = ""TRUE""",
+            "pciBridge4.virtualDev = ""pcieRootPort""",
+            "pciBridge4.functions = ""8""",
+            "tools.syncTime = ""TRUE"""
+        )
         
     }
     catch {
