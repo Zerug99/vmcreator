53355eed1a9b87933a73e1392ec65b709d72c868
unknown <daud357@outlook.com>
2025-05-31 13:58:13 +0200

    kommentarer

--- vmcreator.psm1
+++ vmcreator.psm1
@@ -1,16 +1,20 @@
 function New-VMConfig {
     param (
-        [string]$Name,
-        [int]$CPU,
-        [int]$RAM,
-        [int]$Disk,
-        [string]$ISO
+        [string]$Name,  # Namn på den virtuella maskinen
+        [int]$CPU,      # Antal CPU-kärnor
+        [int]$RAM,      # RAM i MB
+        [int]$Disk,     # Diskstorlek i GB
+        [string]$ISO    # Sökväg till ISO-fil för OS-installation
     )
     try {
+        # Skapa en mapp för VM:n
         $vmPath = "$env:VM_FOLDER\$Name"
         New-Item -ItemType Directory -Path $vmPath -Force | Out-Null
 
+        # Definiera sökvägen till .vmx-konfigurationsfilen
         $vmxPath = "$vmPath\$Name.vmx"
+
+        # Array med parametrar till .vmx-filen
         $vmxContent = @(
             'encoding = "UTF-8"',
             'config.version = "8"',
@@ -36,27 +40,30 @@ function New-VMConfig {
             "tools.syncTime = ""TRUE"""
         )
 
+        # Skriv innehållet till .vmx-filen
         $vmxContent | Set-Content -Path $vmxPath -Encoding UTF8
 
+        # Skapa VMDK-disk med VMware's diskverktyg
         Push-Location $vmPath
         & $env:VMWARE_VDISKMANAGER -c -s ${Disk}GB -a lsilogic -t 0 "$Name.vmdk"
         Pop-Location
     }
     catch {
+        # Felhantering
         throw "Misslyckades med att skapa VM-konfiguration: $_"
     }
 }
 
 function Start-VM {
-    param ([string]$Name)
-    $vmxPath = "$env:VM_FOLDER\$Name\$Name.vmx"
-    & $env:VMRUN_PATH start $vmxPath nogui
+    param ([string]$Name)                           # Namn på den VM som ska startas
+    $vmxPath = "$env:VM_FOLDER\$Name\$Name.vmx"     # Sökväg till VM-konfigurationen
+    & $env:VMRUN_PATH start $vmxPath nogui          # Starta VM i bakgrunden utan GUI
 
 
     # Öppna även i Workstation GUI för att synas i listan
     Start-Sleep -Seconds 1
     & $env:VMWARE_GUI_PATH $vmxPath
-    Write-Log -Message "VM '$Name' öppnades i VMware GUI."
+    Write-Log -Message "VM '$Name' öppnades i VMware GUI."     # Logga att VM startades
 }
 
 
